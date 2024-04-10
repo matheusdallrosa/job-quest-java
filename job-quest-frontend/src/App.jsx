7f5b2745effa0969efa14578b3d5acaae9a3fd38
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-04-11 00:12:03 +0530

    feat: Configure app router

--- job-quest-frontend/src/App.jsx
+++ job-quest-frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import Home from "./pages/Home";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Home />;
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <div className="min-h-screen flex flex-col justify-between bg-slate-950">
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- job-quest-frontend/src/pages/Home.jsx
+++ job-quest-frontend/src/pages/Home.jsx
@@ -1,16 +1,11 @@
 import HeroSection from "../components/HeroSection";
 import ButtonsSection from "../components/ButtonsSection";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-between bg-slate-950">
-      <div className="pt-40 px-32">
-        <HeroSection />
-        <ButtonsSection />
-      </div>
-
-      <Footer />
+    <div className="pt-40 px-32">
+      <HeroSection />
+      <ButtonsSection />
     </div>
   );
 };
