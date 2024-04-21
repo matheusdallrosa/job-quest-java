a088877cde85ed204afb1515cf4653c87fdb0b58
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-04-21 20:11:40 +0530

    style: Minor style updates according to job application status

--- job-quest-frontend/src/components/ApplicationsSection.jsx
+++ job-quest-frontend/src/components/ApplicationsSection.jsx
@@ -7,10 +7,13 @@ const ApplicationsSection = () => {
   const isRecruiter = useSelector((state) => state.auth.isRecruiter);
   const userData = useSelector((state) => state.auth.userData);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [applications, setApplications] = useState([]);
 
   useEffect(() => {
     const fetchApplications = async () => {
+      setIsLoading(true);
+
       const applicationsResponse = await api.get("/api/v1/applications");
       const jobsResponse = await api.get("/api/v1/jobs");
 
@@ -56,6 +59,8 @@ const ApplicationsSection = () => {
 
         setApplications(formattedApplications);
       }
+
+      setIsLoading(false);
     };
 
     fetchApplications();
@@ -64,58 +69,67 @@ const ApplicationsSection = () => {
   if (isRecruiter) {
     return (
       <div className="my-10">
-        <h1 className="text-white text-2xl font-bold">Applications</h1>
+        <h1 className="text-white text-2xl font-bold">Pending Applications</h1>
 
         <div className="my-8 flex flex-col gap-6 text-white">
-          {applications.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-between divide-x-2 border rounded-lg"
-            >
-              <div className="p-6 w-4/5">
-                <div className="mb-6">
-                  <p className="font-semibold">
-                    {item.name}{" "}
-                    <span className="ml-10 text-sm opacity-80">
-                      {item.qualification}
-                    </span>
-                  </p>
-                  <p>{item.position}</p>
-                </div>
-                <div>
-                  <p className="opacity-80">{item.email}</p>
-                  <p className="opacity-80">{item.phone}</p>
-
-                  <p className="my-4">
-                    {item.skills.map((skill, idx) => (
-                      <span
-                        key={idx}
-                        className="mr-2 py-1 px-2 bg-slate-700 text-xs border rounded-md"
-                      >
-                        {skill}
-                      </span>
-                    ))}
-                  </p>
-
-                  <a
-                    href={item.resumeLink}
-                    target="_blank"
-                    className="underline"
-                  >
-                    Resume
-                  </a>
-                </div>
-              </div>
-              <div className="px-6 w-1/5 flex flex-col flex-grow justify-evenly items-center text-white">
-                <button className="py-6 px-8 bg-green-600 hover:opacity-70 rounded-lg text-white text-lg font-semibold transition-opacity">
-                  Accept
-                </button>
-                <button className="py-6 px-8 bg-red-600 hover:opacity-70 rounded-lg text-white text-lg font-semibold transition-opacity">
-                  Reject
-                </button>
-              </div>
+          {isLoading ? (
+            <div>
+              <p className="my-10 text-lg font-semibold">Loading...</p>
             </div>
-          ))}
+          ) : (
+            applications.map(
+              (item) =>
+                item.status === "Pending" && (
+                  <div
+                    key={item.id}
+                    className="flex justify-between divide-x-2 border rounded-lg"
+                  >
+                    <div className="p-6 w-4/5">
+                      <div className="mb-6">
+                        <p className="font-semibold">
+                          {item.name}{" "}
+                          <span className="ml-10 text-sm opacity-80">
+                            {item.qualification}
+                          </span>
+                        </p>
+                        <p>{item.position}</p>
+                      </div>
+                      <div>
+                        <p className="opacity-80">{item.email}</p>
+                        <p className="opacity-80">{item.phone}</p>
+
+                        <p className="my-4">
+                          {item.skills.map((skill, idx) => (
+                            <span
+                              key={idx}
+                              className="mr-2 py-1 px-2 bg-slate-700 text-xs border rounded-md"
+                            >
+                              {skill}
+                            </span>
+                          ))}
+                        </p>
+
+                        <a
+                          href={item.resumeLink}
+                          target="_blank"
+                          className="underline"
+                        >
+                          Resume
+                        </a>
+                      </div>
+                    </div>
+                    <div className="px-6 w-1/5 flex flex-col flex-grow justify-evenly items-center text-white">
+                      <button className="py-6 px-8 bg-green-600 hover:opacity-70 rounded-lg text-white text-lg font-semibold transition-opacity">
+                        Accept
+                      </button>
+                      <button className="py-6 px-8 bg-red-600 hover:opacity-70 rounded-lg text-white text-lg font-semibold transition-opacity">
+                        Reject
+                      </button>
+                    </div>
+                  </div>
+                )
+            )
+          )}
         </div>
       </div>
     );
@@ -127,7 +141,11 @@ const ApplicationsSection = () => {
 
       <div className="p-4 my-4 border rounded-lg text-white">
         <div className="flex flex-col gap-2 divide-y divide-white/40">
-          {applications.length > 0 ? (
+          {isLoading ? (
+            <div>
+              <p className="my-10 text-lg font-semibold">Loading...</p>
+            </div>
+          ) : applications.length > 0 ? (
             applications.map((item) => (
               <div
                 key={item.id}
@@ -143,7 +161,15 @@ const ApplicationsSection = () => {
                   </p>
                 </div>
                 <div>
-                  <p>{item.status}</p>
+                  <p
+                    className={`${
+                      item.status === "Accepted"
+                        ? "text-green-500"
+                        : item.status === "Rejected" && "text-red-500"
+                    }`}
+                  >
+                    {item.status}
+                  </p>
                 </div>
               </div>
             ))
